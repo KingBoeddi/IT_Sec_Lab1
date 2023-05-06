2230edf8cfcd243333c9b5180ecbccea2da228e0
Robert Fesko <rofeit00@hs-esslingen.de>
2023-05-06 09:39:34 +0200

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -27,19 +27,40 @@ CREATE TABLE User(
 
 -- Insert new users
 INSERT INTO User (username, password)
-VALUES
-    ('admin', 'tops3cret'),
+VALUES ('admin', 'tops3cret'),
     ('jane_doe', 'jane123'),
     ('john_smith', 'john456'),
     ('alice_brown', 'alice789');
 
 -- Insert notes for each user
 INSERT INTO Note (user_id, title, content)
-VALUES
-    (1, 'Grocery List', 'Milk, eggs, bread, apples, bananas, orange juice'),
-    (1, 'Books to Read', 'The Catcher in the Rye, To Kill a Mockingbird, 1984, Brave New World, The Great Gatsby'),
-    (2, 'Workout Routine', 'Monday: Chest and Triceps, Wednesday: Back and Biceps, Friday: Legs and Shoulders'),
-    (2, 'Upcoming Meetings', 'Project meeting on Monday at 10am, Client call on Wednesday at 3pm, Team lunch on Friday at 12pm'),
-    (3, 'Birthday Gift Ideas', 'Smart speaker, Wireless headphones, Books, Gift cards, Subscription box'),
-    (3, 'Weekend Plans', 'Friday: Dinner with friends, Saturday: Visit museum, Sunday: Relax at home');
-
+VALUES (
+        1,
+        'Grocery List',
+        'Milk, eggs, bread, apples, bananas, orange juice'
+    ),
+    (
+        1,
+        'Books to Read',
+        'The Catcher in the Rye, To Kill a Mockingbird, 1984, Brave New World, The Great Gatsby'
+    ),
+    (
+        2,
+        'Workout Routine',
+        'Monday: Chest and Triceps, Wednesday: Back and Biceps, Friday: Legs and Shoulders'
+    ),
+    (
+        2,
+        'Upcoming Meetings',
+        'Project meeting on Monday at 10am, Client call on Wednesday at 3pm, Team lunch on Friday at 12pm'
+    ),
+    (
+        3,
+        'Birthday Gift Ideas',
+        'Smart speaker, Wireless headphones, Books, Gift cards, Subscription box'
+    ),
+    (
+        3,
+        'Weekend Plans',
+        'Friday: Dinner with friends, Saturday: Visit museum, Sunday: Relax at home'
+    );
